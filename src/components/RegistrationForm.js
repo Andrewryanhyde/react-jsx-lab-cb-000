466471b0e2c1fff6aa3f7626c1c057b5724b008d
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-24 09:46:54 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+class RegistrationForm extends React.component {
+  render() {
+    return (
+      <form>
+        <input type="text" name="name"/>
+        <inout type="password" name="password"/>
+        <button type="submit" value="submit"/>
+      </form>
+    )
+  }
+}
